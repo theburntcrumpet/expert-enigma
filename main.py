a21845e6220032edb744cc672952d6705ca1c787
theburntcrumpet <msksansom@gmail.com>
2018-11-08 15:53:44 +0000

    Tidied up a little

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ MAX_ITEMS_SELECT = 15
 
 days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 WORK_DAYS = ["Tuesday","Wednesday","Thursday"]
-WORK_START_TIME = datetime.time(hour=8,minute=30)
-WORK_END_TIME = datetime.time(hour=17,minute=0)
+WORK_START_TIME = datetime.time(hour=8,minute=40)
+WORK_END_TIME = datetime.time(hour=16,minute=50)
 LUNCH_HOUR = datetime.time(hour=12,minute=12)
 LUNCH_HOUR_LENGTH = datetime.timedelta(hours=1)
 
@@ -22,6 +22,7 @@ def CurrentlyWorking():
     # Is today a working day for you?
     if days[currentNow.weekday()] not in WORK_DAYS:
         return False
+        
     # Are we outside of working hours?
     if currentNow.time() < WORK_START_TIME or currentNow.time() > WORK_END_TIME: 
         return False
